9db1c65045b87bde0532561318ae2651eaad7c89
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-10-12 00:18:54 -0700

    half improve code quality

--- index.ts
+++ index.ts
@@ -3,14 +3,16 @@ import "frida-il2cpp-bridge";
 Il2Cpp.perform(() => {
     const neptune = Il2Cpp.domain.assembly("Neptune.Core").image;
     const ntcore = neptune.class("NTCore.CommonAPI")
-    const nt1 = neptune.class("NTCore.CommonAPI").nested("__Observable")
+    const requestClass = ntcore.nested("__Observable")
     const getDomain = ntcore.method("GetDomainURL");
 
     // modify this to the server you are redirecting to
     const SERVER_URL = "http://localhost:5000"
+
+    // dump of it
     //     static System.IObservable<LitJson.JsonData> RequestAsObservable(System.String serverAddr, System.String act, NTBase.HttpRequestParam param, NTBase.WebData webData, System.Func<NTBase.WebData,NTBase.WebData> preprocessor, System.Func<NTCore.HttpResponse,System.String> postprocessor); // 0x00d2ab84
 
-    const method = nt1.method("RequestAsObservable").overload("System.String", "System.String", "NTBase.HttpRequestParam", "NTBase.WebData", "System.Func<NTBase.WebData,NTBase.WebData>", "System.Func<NTCore.HttpResponse,System.String>");
+    const requestRedirect = requestClass.method("RequestAsObservable").overload("System.String", "System.String", "NTBase.HttpRequestParam", "NTBase.WebData", "System.Func<NTBase.WebData,NTBase.WebData>", "System.Func<NTCore.HttpResponse,System.String>");
 
 
     getDomain.implementation = function (keyName: Il2Cpp.String) : any {
@@ -18,7 +20,7 @@ Il2Cpp.perform(() => {
         return Il2Cpp.string(SERVER_URL)
     }
     
-    method.implementation = function (serverAddr: Il2Cpp.String, act: Il2Cpp.String, param: Il2Cpp.Parameter, webData: Il2Cpp.Object, preprocessor: any, postprocessor: any) : any {
+    requestRedirect.implementation = function (serverAddr: Il2Cpp.String, act: Il2Cpp.String, param: Il2Cpp.Parameter, webData: Il2Cpp.Object, preprocessor: any, postprocessor: any) : any {
         serverAddr = Il2Cpp.string(SERVER_URL);
         const result = this.method("RequestAsObservable").overload("System.String", "System.String", "NTBase.HttpRequestParam", "NTBase.WebData", "System.Func<NTBase.WebData,NTBase.WebData>", "System.Func<NTCore.HttpResponse,System.String>").invoke(serverAddr, act, param, webData, preprocessor, postprocessor);
         return result;
